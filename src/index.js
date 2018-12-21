bf02c32b55d35d7c38826811f63624e836f7ecae
Sonu <sonusaw46@gmail.com>
2018-12-21 16:10:07 +0545

    first commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import LinkedIn from './pages/Navbar/navbar';
+import * as serviceWorker from './serviceWorker';
+// import {MuiThemeProvider } from '@material-ui/core/styles'
+// import theme from './theme'
+
+// ReactDOM.render(<MuiThemeProvider theme={theme}><SimpleModal /></MuiThemeProvider>, document.getElementById('root'));
+ReactDOM.render(<LinkedIn /> , document.getElementById('root'));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: http://bit.ly/CRA-PWA
+serviceWorker.unregister();
---
+++ src/theme.js
@@ -0,0 +1,10 @@
+import { createMuiTheme } from '@material-ui/core/styles'
+
+const theme = createMuiTheme({
+    palette: {
+        primary: { main: '#11cb5f' },
+        secondary: { main: '#ffcccc' },
+    },
+});
+
+export default theme
